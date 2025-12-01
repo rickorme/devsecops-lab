c7f85bfbe28e5f6b96c857873aa28ed8ab605f9b
rickorme <rick@ampyourlight.com>
2025-12-01 20:14:02 +0000

    Basic "Hello World" API endpoint created

---
+++ main.py
@@ -0,0 +1,10 @@
+import uvicorn
+
+def main():
+    print("Starting test app...")
+
+    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
+    
+
+if __name__ == "__main__":
+    main()
